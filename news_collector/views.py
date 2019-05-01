1a2bd6ce082b27d581db494f2c64d10f7b262042
wspalding <thisisspalding@gmail.com>
2019-05-01 13:33:03 -0700

    models test 2

--- news_collector/views.py
+++ news_collector/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from news_collector.models import Test
 
 
 def index(request):
-    return HttpResponse("working")
+    tests = Test.objects.all()
+    test2 = Test.objects.get_or_create(name="test2")
+    all_tests = [x.name for x in tests]
+    return JsonResponse({"test models": all_tests})
