9c7c58fa05c98d8f7480d9d960b4178dffde963b
wspalding <thisisspalding@gmail.com>
2019-05-02 20:08:02 -0700

    500 error test 1

--- cozzez_site_1/settings.py
+++ cozzez_site_1/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     # white noise for serving static files
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+              #'whitenoise.middleware.WhiteNoiseMiddleware',
     # more django
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
